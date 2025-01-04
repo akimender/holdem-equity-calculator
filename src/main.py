95d874cbdfe7d16914e2b62ef052d824005fc6fc
akimender <akimender@gmail.com>
2025-01-04 00:19:15 -0500

    almost finished referee function

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from hand import Hand
 from card import Card
 from river import River
 
-hand_1 = Hand(Card(1, "Hearts"), Card(7, "Spades"))
+hand_1 = Hand(Card(1, "Hearts"), Card(11, "Spades"))
 hand_2 = Hand(Card(6, "Diamonds"), Card(7, "Diamonds"))
 river = River([Card(2, "Hearts"), Card(3, "Clubs"), Card(5, "Spades"), Card(11, "Clubs"), Card(12, "Diamonds")])
 
